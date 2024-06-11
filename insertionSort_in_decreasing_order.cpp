a1fcde5ec76fa3b7249c4afe4a66da6cb76f27bc
Abhishek Ranjan <119099659+abhi-2029@users.noreply.github.com>
2024-06-11 15:41:51 +0530

    Add files via upload

---
+++ insertionSort_in_decreasing_order.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void print(vector<int>&v){
+    for(int i=0; i<v.size(); i++){
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+}
+void InsertionSort(vector<int>&v){
+    int n= v.size();
+    for(int i=1; i<n; i++){
+        int key=v[i];
+        int j =i-1;
+        while(j>=0 && j<key){
+            v[j+1]=v[j];
+            j--;
+        }
+        v[j+1]=key;
+    }
+}
+int main(){
+    vector<int>v={1,2,3,4,5};
+    InsertionSort(v);
+    print(v);
+    return 0;
+}
